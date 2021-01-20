9a5526c0c456032ecea5e7bf9a90d677fa139130
Karan Daftary <karanda@microsoft.com>
2021-01-19 18:38:50 -0800

    making Program work with appsettings.json file locally

---
+++ reliability-on-demand/Extensions/KeyValueSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace reliability_on_demand.Extensions
+{
+    public class KeyVaultSettings
+    {
+        public string Vault { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+    }
+}
--- reliability-on-demand/Program.cs
+++ reliability-on-demand/Program.cs
@@ -1,11 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using reliability_on_demand.Extensions;
+using Azure.Identity;
+
 
 namespace reliability_on_demand
 {
@@ -13,14 +18,26 @@ namespace reliability_on_demand
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+        public static IWebHost CreateHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    config.SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", optional: false)
+                        .AddEnvironmentVariables();
+
+                    var builtConfig = config.Build();
+                    var settings = builtConfig.GetSection("KeyVault").Get<KeyVaultSettings>();
+
+                    config.AddAzureKeyVault(
+                        settings.Vault, settings.ClientId, settings.ClientSecret);
+
+                })
+                //.UseApplicationInsights() --> this used to work in Microsoft.AspNetCore 2.0 ----> need to check how it translates to 3.0
+                .UseStartup<Startup>()
+                .Build();
     }
 }
