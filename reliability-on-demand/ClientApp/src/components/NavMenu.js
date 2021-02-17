90162c4e3d7c0ab17a2e808a7e979034942245ab
Karan Daftary <karanda@microsoft.com>
2021-02-16 19:29:07 -0800

    adding SQLData page

--- reliability-on-demand/ClientApp/src/components/NavMenu.js
+++ reliability-on-demand/ClientApp/src/components/NavMenu.js
@@ -37,7 +37,10 @@ export class NavMenu extends Component {
                   <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Kusto data</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink tag={Link} className="text-dark" to="/sql-data">SQL data</NavLink>
                 </NavItem>
               </ul>
             </Collapse>
---
+++ reliability-on-demand/ClientApp/src/components/SQLData.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+function SQLData(){
+    return(
+        <h1> Hello World - from SQLData page</h1>
+    )
+}
+
+export {
+    SQLData
+}
