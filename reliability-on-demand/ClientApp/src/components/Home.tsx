a5904b4c63b24c6b97d74b96248be380808b2fd6
Karan Daftary <karanda@microsoft.com>
2021-02-17 18:23:12 -0800

    Converted home page to typescript

--- reliability-on-demand/ClientApp/src/components/Home.tsx
+++ reliability-on-demand/ClientApp/src/components/Home.tsx
@@ -1,16 +1,17 @@
-import React, { Component } from "react";
+import React, { ReactElement } from 'react'
 
-export class Home extends Component {
+interface Props {
+  
+}
 
-  render() {
-    return (
-      <div>
+export const Home = (props: Props) : ReactElement => {
+  return (
+    <div>
         <h1>Welcome to Reliability on Demand</h1>
         <p>
           To get started click on Fetch data to see a list of releases from
           Kusto.
         </p>
       </div>
-    );
-  }
+  )
 }
