248bd268d0873412909eb2f367738c3605cfc102
Karan Daftary <karanda@microsoft.com>
2021-03-25 16:26:17 -0700

    add stack to configPage

--- reliability-on-demand/ClientApp/src/components/Config/ConfigPage.tsx
+++ reliability-on-demand/ClientApp/src/components/Config/ConfigPage.tsx
@@ -1,12 +1,24 @@
 import * as React from 'react';
-import { createTheme, ITheme, PrimaryButton} from '@fluentui/react';
-import  StudySection  from './Study/StudySection';
+import { createTheme, DefaultPalette, IStackItemStyles, IStackStyles, IStackTokens, ITheme, PrimaryButton, Stack } from '@fluentui/react';
+import StudySection from './Study/StudySection';
 import TeamSection from './Team/TeamSection';
 
 export interface IConfigProps {
   //TODO pass in the current user
 }
 
+export function ConfigPage() {
+  return (
+    <Stack tokens={containerStackTokens}>
+      <TeamSection />
+      <StudySection inquiry={{ TeamID: 3 }} />
+      <PrimaryButton>Submit</PrimaryButton>
+    </Stack>
+  );
+}
+
+
+// Separator related styles
 export const largeTitle: ITheme = createTheme({
   fonts: {
     medium: {
@@ -14,15 +26,6 @@ export const largeTitle: ITheme = createTheme({
     },
   },
 });
+// Stack related stuff
+const containerStackTokens: IStackTokens = { childrenGap: 10 };
 
-export function ConfigPage(props: IConfigProps) {
-  return (
-    <div>
-      <TeamSection />
-
-      <StudySection inquiry={{TeamID:3}}/>
-
-      <PrimaryButton>Submit</PrimaryButton>
-    </div>
-  );
-}
