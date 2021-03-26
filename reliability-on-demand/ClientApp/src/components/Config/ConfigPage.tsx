b2863e9925eec4251e7b83012e5da66d08866515
Karan Daftary <karanda@microsoft.com>
2021-03-25 20:37:22 -0700

    converted Config Page function to Class component

--- reliability-on-demand/ClientApp/src/components/Config/ConfigPage.tsx
+++ reliability-on-demand/ClientApp/src/components/Config/ConfigPage.tsx
@@ -7,21 +7,48 @@ import TeamSection from '../Team/TeamSection';
 import { containerStackTokens } from '../helpers/Styles';
 import { Stack } from '@fluentui/react';
 import { PrimaryButton } from '@fluentui/react';
+import { ConfigInquiry, TeamConfig } from '../../models/config.model';
 
 export interface IConfigProps {
   //TODO pass in the current user
 }
 
-export function ConfigPage() {
-  return (
-    <Stack tokens={containerStackTokens}>
-      <TeamSection />
-      <StudySection inquiry={{ TeamID: 3 }} />
-      <PrimaryButton>Submit</PrimaryButton>
-    </Stack>
-  );
+export interface IConfigState {
+  currentTeam?: TeamConfig;
 }
 
+export class ConfigPage extends React.Component<IConfigProps, IConfigState> {
+  constructor(props: IConfigProps) {
+    super(props)
+  }
 
+  componentDidMount() {
+    this.setState({
+      currentTeam: undefined
+    })
+  }
 
+  render() {
+    return (
+      <Stack tokens={containerStackTokens}>
+        <TeamSection printHello={this.printTeam} />
+        <StudySection inquiry={{ TeamID: this.extractTeamIDFromState()}} />
+        <PrimaryButton>Submit</PrimaryButton>
+      </Stack>
+    );
+  }
+
+  extractTeamIDFromState():number{
+    return 3;
+  }
+
+  printTeam = (selectedTeam: TeamConfig) => {
+    console.log(selectedTeam);
+    this.setState(
+      {
+        currentTeam: selectedTeam
+      }
+    );
+  }
+}
 
