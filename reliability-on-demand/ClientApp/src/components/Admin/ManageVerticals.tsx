df94380e1600abfb10dde19509a82a5609262966
Anjali Goel <anjgoel@microsoft.com>
2022-08-31 17:52:15 +0000

    Merged PR 7789177: Adding verticals multi-select dropdown
    Adding verticals multi-select dropdown
    
    Related work items: #34069427

--- reliability-on-demand/ClientApp/src/components/Admin/ManageVerticals.tsx
+++ reliability-on-demand/ClientApp/src/components/Admin/ManageVerticals.tsx
@@ -1,7 +1,61 @@
+import { IComboBoxOption } from "@fluentui/react"
+import axios from "axios"
 import * as React from "react"
+import { useEffect, useState } from "react"
+import { Loading } from "../helpers/Loading"
+import { MyMultiSelectComboBox } from "../helpers/MyMultiSelectComboBox"
 
 export interface IManageVerticalsProps {}
 
 export const ManageVerticals = (props: IManageVerticalsProps) => {
-    return <div />
+    const [loading, setLoading] = useState<boolean>(false)
+    const [verticals, setVerticals] = useState<IComboBoxOption[]>([])
+    const [selectedVerticals, setSelectedVerticals] = useState<IComboBoxOption[]>([])
+
+    useEffect(() => {
+        setLoading(true)
+        axios
+            .get(`api/Data/GetAllVerticals`)
+            .then((response) => {
+                if (response.data) {
+                    const arr = response.data
+                    const ans = arr.map((item: any) => {
+                        const rObj = {
+                            key: item.VerticalName.concat("_", item.PivotSourceSubType),
+                            text: item.VerticalName,
+                        }
+                        return rObj
+                    })
+                    setVerticals(ans) // force combobox to show placeholder text by default
+                } else {
+                    setVerticals([])
+                }
+                setLoading(false)
+            })
+            .catch((exception) => {
+                return console.error(exception)
+            })
+    }, [])
+
+    const onVerticalsSelected = (input: any) => {
+        setSelectedVerticals(input)
+    }
+
+    return (
+        <div>
+            {loading ? (
+                <Loading message="Getting Data for Admin Modify Vertical Section - hang tight" />
+            ) : (
+                <div>
+                    <MyMultiSelectComboBox
+                        label="Verticals"
+                        options={verticals}
+                        placeholder="Type a vertical name or select verticals from the list"
+                        selectedItems={selectedVerticals}
+                        callback={onVerticalsSelected}
+                    />
+                </div>
+            )}
+        </div>
+    )
 }
---
+++ reliability-on-demand/ClientApp/src/components/Admin/VerticalsDetailedList.tsx
@@ -0,0 +1,63 @@
+import React from "react"
+import {
+    DetailsList,
+    TooltipHost,
+    SelectionMode,
+    IColumn,
+    Checkbox,
+    IDropdownOption
+} from "@fluentui/react"
+import { Pivot } from "../../models/pivot.model"
+import { mapPivotTableColumnValue, buildColumnArray } from "./helper"
+
+// Please ignore this for now. I will be changing this file in this task - 41135957
+interface Props {
+    data: Pivot[]
+    callBack: any
+}
+
+export const PivotsDetailedList = (props: Props) => {
+    const renderItemColumn = (item: IDropdownOption, index?: number, column?: IColumn) => {
+        const fieldContent = item[column?.fieldName as keyof IDropdownOption] as string
+
+        if (
+            column?.key === "VerticalName"
+        ) {
+            return <span>{fieldContent}</span>
+        }
+
+            return (
+                <span>
+                    <Checkbox
+                        checked={Boolean(fieldContent ?? false)}
+                        id={`${index}_${column?.key}`}
+                        onChange={onCheckboxChange}
+                    />
+                </span>
+            )
+    }
+
+    const onCheckboxChange = (
+        ev?: React.FormEvent<HTMLElement | HTMLInputElement>,
+        isChecked?: boolean
+    ) => {
+        const target = ev?.target as HTMLInputElement
+        const arr = target.id.toString().split("_")
+        const row = Number(arr[0])
+        const col = arr[1]
+
+        props.callBack(mapPivotTableColumnValue(props.data, row, col, target.checked))
+    }
+
+    return (
+        <div>
+                <DetailsList
+                    items={props.data}
+                    setKey="set"
+                    columns={buildColumnArray(props.data)}
+                    onRenderItemColumn={renderItemColumn}
+                    selectionMode={SelectionMode.none}
+                />
+        </div>
+    )
+}
